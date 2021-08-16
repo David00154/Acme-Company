0f089878935dc6868a8b610cc1f471700cb8778c
david00154 <davidbriggs00154@gmail.com>
2021-08-16 08:30:58 +0100

    new commit

---
+++ components/Slider.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="slider">
+    <div v-for="(slide, index) in slides" :key="index">
+      <div class="slide" :style="`background-image: url(${slide.image})`">
+        <div class="slide-content">
+          <h1>Slider 1</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import "boxicons";
+import Vue from "vue";
+export default Vue.extend({
+  name: "slider",
+  data() {
+    return {
+      slides: [
+        {
+          image: `${require("~/assets/images/img1.jpg")}`,
+        },
+      ],
+    };
+  },
+});
+</script>
+
+<style scoped>
+.slider {
+  width: 100vw;
+  height: 80vh;
+  overflow-x: hidden;
+}
+.slide {
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  height: 100%;
+  width: 100%;
+}
+/*
+prefix width css
+sm     640   @media (min-width: 640px)
+md     768   @media (min-width: 768px)
+lg     1024   @media (min-width: 1024px)
+xl     1280   @media (min-width: 1280px)
+2xl     1536   @media (min-width: 1536px)
+*/
+</style>
---
+++ layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <slider />
+    <nuxt />
+  </div>
+</template>
+
+<script lang="ts">
+// import Slider from '~/components/Slider.vue';
+export default {
+  //   components: { Slider },
+};
+</script>
+
+<style>
+</style>
